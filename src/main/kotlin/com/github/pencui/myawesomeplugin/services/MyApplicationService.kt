8d8e7b3939c82099f4c7f726ece4887fb531067a
GitHub Action <action@github.com>
2021-07-02 02:53:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/pencui/myawesomeplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/pencui/myawesomeplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.pencui.myawesomeplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pencui.myawesomeplugin.MyBundle
 
 class MyApplicationService {
 
